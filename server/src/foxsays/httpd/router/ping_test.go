10f5d943e3bd973c2519e8849a03187277137da0
Levi Cook <levicook@gmail.com>
2014-01-12 20:07:53 -0700

    ping handler: minor test refactoring

--- server/src/foxsays/httpd/router/ping_test.go
+++ server/src/foxsays/httpd/router/ping_test.go
@@ -9,11 +9,11 @@ import (
 
 func Test_ping(t *testing.T) {
 	w := httptest.NewRecorder()
-	r, err := http.NewRequest("GET", "/ping", nil)
-	assert.Nil(t, err)
+	r, e := http.NewRequest("GET", "/ping", nil)
+	assert.Nil(t, e)
 
 	ping(w, r)
 
 	assert.Equal(t, 200, w.Code)
-	assert.Equal(t, "pong", w.Body.String())
+	assert.Equal(t, pong, w.Body.Bytes())
 }
