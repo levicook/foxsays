b03e38665837fff82ae4dc9033de6b51fb1e5e33
Levi Cook <levicook@gmail.com>
2014-01-24 22:25:21 -0700

    minor refactorings

--- server/src/foxsays/main.go
+++ server/src/foxsays/main.go
@@ -4,38 +4,23 @@ import (
 	"fmt"
 	"foxsays/config"
 	"foxsays/httpd"
+	"foxsays/log"
 	"github.com/spf13/cobra"
-	"os"
 	"path"
 )
 
-var rootCommand = &cobra.Command{
-	Use:   "foxsays",
-	Short: "foxsays is a twitter clone",
-	Long:  ``,
-}
-
-func init() {
+func main() {
+	rootCommand := &cobra.Command{
+		Use:   "foxsays",
+		Short: "foxsays is a twitter clone"}
 
 	rootCommand.PersistentFlags().StringVarP(
-		&config.File,
-		"config", "c",
-		path.Join(config.AppRoot, "config", fmt.Sprintf("%s.toml", config.AppEnv)),
-		"")
+		&config.File, "config", "c",
+		path.Join(config.AppRoot, "config", fmt.Sprintf("%s.toml", config.AppEnv)), "")
 
 	rootCommand.AddCommand(&cobra.Command{
-		Use:   "httpd",
-		Short: "short help on httpd ...",
-		Long:  "longer help on httpd ...",
-		Run:   httpd.Run,
-	})
+		Use: "httpd",
+		Run: httpd.Run})
 
-}
-
-func main() {
-	if err := rootCommand.Execute(); err != nil {
-		os.Exit(1)
-	} else {
-		os.Exit(0)
-	}
+	log.FatalIf(rootCommand.Execute())
 }
