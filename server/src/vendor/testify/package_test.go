c35e53bb74326cf820d8cfc35036dc490c64e299
Levi Cook <levicook@gmail.com>
2014-02-01 18:06:26 -0700

    Vendor github.com/stretchr/testify revision 9cc77fa2

---
+++ server/src/vendor/testify/package_test.go
@@ -0,0 +1,12 @@
+package testify
+
+import (
+	"testing"
+	"vendor/testify/assert"
+)
+
+func TestImports(t *testing.T) {
+	if assert.Equal(t, 1, 1) != true {
+		t.Error("Something is wrong.")
+	}
+}
